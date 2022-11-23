232f7c94ce6d3f8e483482147803282688410e2a
Universenn <dnwndhks98@gmail.com>
2022-11-23 13:18:45 +0900

    first commit

---
+++ src/main/java/com/example/jpaspringboot1123/JpaSpringboot1123Application.java
@@ -0,0 +1,13 @@
+package com.example.jpaspringboot1123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSpringboot1123Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaSpringboot1123Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/jpaspringboot1123/entity/Book.java
@@ -0,0 +1,5 @@
+package com.example.jpaspringboot1123.entity;
+
+public class Book {
+
+}
